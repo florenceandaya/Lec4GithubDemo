0beb4f0540926e64151e652098630e93124b7188
florenceandaya1095@gmail.com <Fma@5901>
2023-10-03 09:59:52 -0700

    Commit Made some changes locally the following week Oct 03

--- app/src/main/java/com/example/lec4githubdemo/MainActivity.java
+++ app/src/main/java/com/example/lec4githubdemo/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Made some changes locally the following week
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
